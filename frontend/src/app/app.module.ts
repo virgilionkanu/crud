2aaedcb9471f5d5a38ea9402441eb925cd857b28
Virgílio Cano <virgiliocano53@gmail.com>
2020-06-29 17:02:53 -0300

    Add Method Create Product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,9 +10,12 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
@@ -40,6 +43,8 @@ import { ProductCreateComponent } from "./components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { ProductService } from "./../product.service";
+import { Product } from "../product.model";
 
 @Component({
-  selector: 'app-product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: "",
+    price: null,
+  };
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Product save successfully");
+      this.router.navigate(["/products"]);
+    });
   }
-
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 }
